f99258ef9970abd982058dcb5e880bb842777d54
Justin G. Mitchell <jgmitchell@gmail.com>
2021-09-27 12:49:10 -0600

    Adjust quarterly compound interest formula

--- python/accounting.py
+++ python/accounting.py
@@ -30,3 +30,26 @@ def simple_compound_interest(principal: int, interest: float, periods: int) -> f
     """
     total_value = principal * ((1 + interest)**periods)
     return total_value
+
+
+def quarterly_compound_interest(p: int, i: float, n: int) -> float:
+    """
+    Calculates the total return on a standard deposit and interest rate every period.
+
+    Args
+        principal: amount to be deposited every period
+        interest: expressed in decimal 3% = .03
+        periods: the number of periods to calculate for
+    """
+
+    value = 0
+    total_value = 0
+    periods = n
+
+    while periods > 0:
+        value = p * (1 + i)**periods
+        total_value = total_value + value
+        periods -= 4
+        print(value, total_value)
+    
+    return total_value
