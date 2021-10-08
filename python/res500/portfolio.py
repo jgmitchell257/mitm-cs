cafab815fa22d86b438def45d0c5733a96aec334
Justin G. Mitchell <jgmitchell@gmail.com>
2021-10-08 15:53:08 -0600

    Add - import json

--- python/res500/portfolio.py
+++ python/res500/portfolio.py
@@ -7,6 +7,7 @@ https://pypi.org/project/yfinance/
 https://aroussi.com/post/python-yahoo-finance
 '''
 
+import json
 import pandas as pd
 import plotly.express as px
 import yfinance as yf
@@ -16,6 +17,8 @@ lumn = yf.Ticker("LUMN")
 
 # Get basic information on the company
 info = lumn.info
+with open("info.txt", "w") as f:
+    f.write(json.dumps(info))
 
 # Download stock price history and create dataframe
 df = lumn.history(start="2018-09-01",end="2021-10-01")
